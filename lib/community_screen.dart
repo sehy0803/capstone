e402da138b86dbe62889e9bad958b812047696c6
Minseok Park <jmli11@naver.com>
2023-09-28 19:36:08 +0900

    2023-09-28 커뮤니티화면 폰트, 들여쓰기 추가

--- lib/community_screen.dart
+++ lib/community_screen.dart
@@ -9,15 +9,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('커뮤니티', style: TextStyle(color: Colors.black)),
+          title: Padding(
+            padding: const EdgeInsets.only(left: 16.0), // 왼쪽 들여쓰기 설정
+            child: Text(
+              '커뮤니티',
+              style: TextStyle(color: Colors.black, fontSize: 24.0), // 글꼴 크기 조절
+            ),
+          ),
           backgroundColor: Colors.white,
           centerTitle: false,
           titleSpacing: 0.0,
           actions: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(right: 45.0), // 오른쪽 들여쓰기 설정
               child: IconButton(
-                icon: Icon(Icons.search, color: Colors.black),
+                icon: Padding(
+                  padding: const EdgeInsets.only(left: 8.0), // 검색 아이콘 왼쪽 들여쓰기 설정
+                  child: Icon(Icons.search, color: Colors.black, size: 45.0),
+                ),
                 onPressed: () {
                   Navigator.pop(context);
                 },
