5adf37e241797e565e8c2dd89dd642cd86a458dc
성기범 <133200703+sungkibum@users.noreply.github.com>
2023-09-28 18:17:37 +0900

    2023-09-28 디자인 수정, 회원 탈퇴 버튼 추가, 이름 조건 추가

--- lib/profile_edit.dart
+++ lib/profile_edit.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-// void main() {
-//   runApp(const ProfileEdit());
-// }
+void main() {
+  runApp(const ProfileEdit());
+}
 
 class ProfileEdit extends StatelessWidget {
   const ProfileEdit({super.key});
@@ -13,52 +13,66 @@ class ProfileEdit extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('마이페이지', style: TextStyle(color: Colors.black)), backgroundColor: Colors.white,centerTitle: false, titleSpacing: 0.0,
-        ),
-        body: Container(
+            title: Text('마이페이지', style: TextStyle(color: Colors.black)),
+            backgroundColor: Colors.white),
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(50, 30, 50, 10),
+              child: Column(
 
-          child: Column(
-            children: [
-              Image.asset('profile.jpg', width: 150, height: 150,),
-
-              Column(
-
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('이름', style: TextStyle(fontSize: 30)),
-                  TextField(style: TextStyle(fontSize: 50),),
-                  Text('비밀번호', style: TextStyle(fontSize: 30)),
-                  TextField(obscureText: true, style: TextStyle(),),
-                  Text('이메일', style: TextStyle(fontSize: 30)),
-                  TextField(style: TextStyle(fontSize: 50),),
-                  Text('휴대폰 번호', style: TextStyle(fontSize: 30)),
-                  TextField(style: TextStyle(fontSize: 50, ),
-                  keyboardType: TextInputType.phone,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')) // 휴대폰 번호는 숫자만 입력 가능
-                  ],),
+                  Image.asset('profile.jpg', width: 150, height: 150),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                    children: [
+                      TextField(style: TextStyle(fontSize: 15, height: 3), inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp(r'[가-힣]')),] // 이름은 한글만 입력 가능
+                        , decoration: InputDecoration(labelText: '이름'),),
+                      TextField(style: TextStyle(fontSize: 15, height: 3), obscureText: true, decoration: InputDecoration(labelText: '비밀번호'),),
+                      TextField(style: TextStyle(fontSize: 15, height: 3), errorText: _emailIsValid ? null : '올바른 이메일 주소를 입력하세요.', decoration: InputDecoration(labelText: '이메일'),),
+                      TextField(
+                        style: TextStyle(fontSize: 15, height: 3),
+                        decoration: InputDecoration(labelText: '휴대폰 번호'),
+                        keyboardType: TextInputType.phone,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.allow(RegExp(r'[0-9]')) // 휴대폰 번호는 숫자만 입력 가능
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
-              )
-            ],
-          )
+              ),
+            ),
+
+            // 회원탈퇴 버튼을 바텀앱바 바로 위 가운데에 배치
+            Positioned(
+              bottom: 20,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: TextButton(onPressed: (){}, child: Text('회원탈퇴', style: TextStyle(color : Color(0xff18a4f0)))),
+
+              ),
+            ),
+          ],
         ),
         bottomNavigationBar: BottomAppBar(
           child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.home)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.launch)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.chat)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.person)),
-        ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(onPressed: (){}, icon: Icon(Icons.home)),
+              IconButton(onPressed: (){}, icon: Icon(Icons.launch)),
+              IconButton(onPressed: (){}, icon: Icon(Icons.chat)),
+              IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
+              IconButton(onPressed: (){}, icon: Icon(Icons.person)),
+            ],
+          ),
+        ),
       ),
-      ),
-      ),
-
-
-
-
     );
   }
 }
