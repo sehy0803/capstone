e4e88dddde4d0b7d7be8828962c81bd08c47665f
sehy0803 <sehy0803@gmail.com>
2023-10-22 20:19:45 +0900

    2023-10-22 경매 상품 화면

--- lib/auction_screen.dart
+++ lib/auction_screen.dart
@@ -1,3 +1,4 @@
+import 'package:capstone/auction_register_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,39 +28,14 @@ class AuctionScreen extends StatelessWidget {
               child: Column(
                 children: [
                   // 위젯 추가
-                  Container(
-                    height: 150,
-                    color: Colors.lightBlue,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        Icon(Icons.account_circle, size: 60),
-                        Row(
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("닉네임", style: TextStyle(fontSize: 18)),
-                                Text("등록일 2023-10-18 20:33",
-                                    style: TextStyle(fontSize: 14, color: Colors.grey))
-                              ],
-                            ),
-                            IconButton(
-                              onPressed: (){},
-                              icon: Icon(Icons.favorite, color: Colors.redAccent),
-                              iconSize: 30,
-                              padding: EdgeInsets.zero, // 패딩 설정
-                              constraints: BoxConstraints(), // 패딩 설정
-                            ),
-                          ],
-                        )
+                  AuctionPostImage(),
+                  AuctionUserInfo(),
+                  Line(),
+                  AuctionTitleAndContent(),
+                  Line(),
+                  // 댓글
 
 
-                      ],
-                    ),
-                  )
 
 
 
@@ -78,3 +54,77 @@ class AuctionScreen extends StatelessWidget {
 
 // ========================================== 커스텀 위젯 ==========================================
 
+// 경매 게시글에 등록된 이미지
+class AuctionPostImage extends StatelessWidget {
+  const AuctionPostImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      color: Colors.lightBlue,
+    );
+  }
+}
+
+
+// 경매를 등록한 사용자 정보
+class AuctionUserInfo extends StatelessWidget {
+  const AuctionUserInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Icon(Icons.account_circle, size: 60),
+          SizedBox(width: 10),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("닉네임", style: TextStyle(fontSize: 18)),
+                    Text("등록일 2023-10-22", style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  ],
+                ),
+                IconButton(
+                  onPressed: (){},
+                  icon: Icon(Icons.favorite, color: Colors.redAccent),
+                  iconSize: 30,
+                  padding: EdgeInsets.zero, // 패딩 설정
+                  constraints: BoxConstraints(), // 패딩 설정
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// 경매 게시글 제목, 내용
+class AuctionTitleAndContent extends StatelessWidget {
+  const AuctionTitleAndContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("제목", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text("내용", style: TextStyle(fontSize: 16))
+          ],
+        ),
+      ),
+    );
+  }
+}
