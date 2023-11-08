e0848cc03238e693ef128f55a031ede50a06d3ef
성기범 <133200703+sungkibum@users.noreply.github.com>
2023-11-08 15:31:09 +0900

    "2023-11-08 .."

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+<<<<<<< HEAD
 import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+=======
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+>>>>>>> parent of 295220f (2023-10-21 회원가입, 로그인 기능 구현중)
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,17 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
+<<<<<<< HEAD
 #include <firebase_core/firebase_core_plugin_c_api.h>
+=======
+>>>>>>> parent of 295220f (2023-10-21 회원가입, 로그인 기능 구현중)
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+<<<<<<< HEAD
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+=======
+>>>>>>> parent of 295220f (2023-10-21 회원가입, 로그인 기능 구현중)
 }
