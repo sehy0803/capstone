e10e29127ed5cc58d0b700c081d83daab17cc321
성기범 <133200703+sungkibum@users.noreply.github.com>
2023-11-20 20:08:25 +0900

    2023-11-20 채팅목록, 채팅방 디자인 중

--- lib/chat_list_screen.dart
+++ lib/chat_list_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'chat_screen.dart';
 
-class ChatListScreen extends StatelessWidget {
-  ChatListScreen({Key? key});
+class ChatListScreen extends StatefulWidget {
+  const ChatListScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatListScreenState createState() => _ChatListScreenState();
+}
+
+class _ChatListScreenState extends State<ChatListScreen> {
+  final _firestore = FirebaseFirestore.instance;
+  List<ChatRoomInfo> chatRooms = [];
+  String uploaderUID = ''; // 업로더 uid
+  String winningBidderUID = ''; // 낙찰자 uid
 
   @override
   Widget build(BuildContext context) {
@@ -26,25 +37,18 @@ class ChatListScreen extends StatelessWidget {
             return Center(child: Text('채팅 목록을 가져오는 도중 오류가 발생했습니다.'));
           }
 
-          final chatRooms = snapshot.data ?? [];
+          chatRooms = snapshot.data ?? [];
 
           return ListView.builder(
             itemCount: chatRooms.length,
+            itemExtent: 100.0, // 아이템의 높이를 원하는 값으로 조절하세요
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(chatRooms[index].chatTitle),
-                onTap: () {
-                  // 채팅방을 클릭했을 때 ChatScreen으로 이동
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChatScreen(
-                        chatRoomId: chatRooms[index].chatRoomId,
-                        chatTitle: chatRooms[index].chatTitle,
-                      ),
-                    ),
-                  );
-                },
+              return ChatListItem(
+                chatRoomId: chatRooms[index].chatRoomId,
+                chatTitle: chatRooms[index].chatTitle,
+                uploaderUID: chatRooms[index].uploaderUID,
+                winningBidderUID: chatRooms[index].winningBidderUID,
+                onExitChatRoom: exitChatRoom,
               );
             },
           );
@@ -57,53 +61,231 @@ class ChatListScreen extends StatelessWidget {
     List<ChatRoomInfo> chatRooms = [];
 
     try {
+      String currentUserId = FirebaseAuth.instance.currentUser?.uid ?? '';
+
       QuerySnapshot<Map<String, dynamic>> querySnapshot =
-      await FirebaseFirestore.instance.collection('AuctionCommunity').get();  // DB에서 컬렉션 정보 가져오기
+      await FirebaseFirestore.instance.collection('AuctionCommunity').get();
 
       for (QueryDocumentSnapshot<Map<String, dynamic>> doc in querySnapshot.docs) {
         String uploaderUID = doc['uploaderUID'];
         String winningBidderUID = doc['winningBidderUID'];
         String auctionTitle = doc['title'];
+        Timestamp? endTime = doc['endTime'];
 
-        if (uploaderUID.isNotEmpty && winningBidderUID.isNotEmpty && auctionTitle.isNotEmpty) { // 채팅방 생성 조건
+        if ((uploaderUID == currentUserId ||
+            winningBidderUID == currentUserId) &&
+            uploaderUID.isNotEmpty &&
+            auctionTitle.isNotEmpty &&
+            endTime != null) {
+          if (endTime.toDate().isBefore(DateTime.now())) {
+            String chatRoomId = createChatRoomId(uploaderUID, winningBidderUID);
 
-          String chatRoomId = createChatRoomId(uploaderUID, winningBidderUID);  // 채팅방 ID 생성
+            chatRooms.add(ChatRoomInfo(
+              chatRoomId: chatRoomId,
+              chatTitle: auctionTitle,
+              uploaderUID: uploaderUID,
+              winningBidderUID: winningBidderUID,
+            ));
 
-          chatRooms.add(ChatRoomInfo(chatRoomId: chatRoomId, chatTitle: auctionTitle)); // 채팅방 정보 추가
-
-          await createChatMessagesDocument(chatRoomId); // 채팅 메시지 문서 생성
+            // createChatMessagesDocument 함수 호출 시 uploaderUID, winningBidderUID 추가
+            await createChatMessagesDocument(
+                chatRoomId, currentUserId, uploaderUID, winningBidderUID);
+          }
         }
       }
     } catch (e) {
-      print('Error getting chat rooms: $e');
+      print('채팅 목록을 가져오는 중 오류 발생: $e');
     }
 
     return chatRooms;
   }
 
-  Future<void> createChatMessagesDocument(String chatRoomId) async {
+  Future<void> createChatMessagesDocument(String chatRoomId, String currentUserId,
+      String uploaderUID, String winningBidderUID) async {
+    DocumentReference chatMessagesDocRef =
+    FirebaseFirestore.instance.collection('chat_messages').doc(chatRoomId);
 
-    DocumentReference chatMessagesDocRef = FirebaseFirestore.instance
-        .collection('chat_messages')
-        .doc(chatRoomId); // DB의 chat_messages 문서 참조
-
-
-    if (!(await chatMessagesDocRef.get()).exists) { // 문서가 이미 존재하는지 확인
-      await chatMessagesDocRef.set({  // 없는 경우 문서 생성
-        'created_at': FieldValue.serverTimestamp(),
+    try {
+      // "Chat" 컬렉션에 데이터를 직접 설정합니다.
+      await _firestore.collection('Chat').doc(chatRoomId).set({
+        'chatRoomId': chatRoomId,
+        'uploaderUID': uploaderUID,
+        'winningBidderUID': winningBidderUID,
+        // 추가 필드는 여기에 추가할 수 있습니다.
       });
+    } catch (e) {
+      print('Chat 문서 생성 중 오류 발생: $e');
     }
   }
 
   String createChatRoomId(String uid1, String uid2) {
-    List<String> sortedUids = [uid1, uid2]..sort(); // UID 정렬
+    List<String> sortedUids = [uid1, uid2]..sort();
     return '${sortedUids[0]}_${sortedUids[1]}';
+  }
+
+  void exitChatRoom(String chatRoomId) async {
+    try {
+      print('채팅방 나가기: $chatRoomId');
+
+      // Chat 컬렉션에서 해당 채팅방 문서 삭제
+      await FirebaseFirestore.instance.collection('Chat').doc(chatRoomId).delete();
+
+      print('채팅방 삭제 성공');
+
+      // 해당 채팅방을 chatRooms 리스트에서 제거
+      setState(() {
+        chatRooms.removeWhere((chatRoom) => chatRoom.chatRoomId == chatRoomId);
+      });
+    } catch (e) {
+      print('채팅방 나가기 중 오류 발생: $e');
+    }
   }
 }
 
 class ChatRoomInfo {
   final String chatRoomId;
   final String chatTitle;
+  final String uploaderUID;
+  final String winningBidderUID;
 
-  ChatRoomInfo({required this.chatRoomId, required this.chatTitle});
+  ChatRoomInfo({
+    required this.chatRoomId,
+    required this.chatTitle,
+    required this.uploaderUID,
+    required this.winningBidderUID,
+  });
+}
+
+class ChatListItem extends StatelessWidget {
+  final String chatRoomId;
+  final String chatTitle;
+  final String uploaderUID;
+  final String winningBidderUID;
+  final Function(String) onExitChatRoom;
+
+  ChatListItem({
+    required this.chatRoomId,
+    required this.chatTitle,
+    required this.uploaderUID,
+    required this.winningBidderUID,
+    required this.onExitChatRoom,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.blueGrey[50],
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundImage: NetworkImage('상대방 프로필 사진 경로'),
+          ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '$chatTitle',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              FutureBuilder(
+                future: getLastMessage(chatRoomId),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading...');
+                  }
+
+                  if (snapshot.hasError) {
+                    return Text('Error');
+                  }
+
+                  var lastMessage = snapshot.data as String?;
+                  return Text(
+                    lastMessage ?? '',
+                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  );
+                },
+              ),
+            ],
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatScreen(
+                  chatRoomId: chatRoomId,
+                  chatTitle: chatTitle,
+                  uploaderUID: uploaderUID,
+                  winningBidderUID: winningBidderUID,
+                ),
+              ),
+            );
+          },
+          onLongPress: () {
+            showExitChatDialog(context, chatRoomId);
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<String?> getLastMessage(String chatRoomId) async {
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
+          .collection('chat_messages')
+          .doc(chatRoomId)
+          .collection('messages')
+          .orderBy('timestamp', descending: true)
+          .limit(1)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        var latestMessage = querySnapshot.docs.first.data();
+        String sender = latestMessage['senderId'] ?? '';
+        String messageText = latestMessage['text'] ?? '';
+
+        return '$sender: $messageText';
+      }
+
+      return null;
+    } catch (e) {
+      print('마지막 메시지를 가져오는 중 오류 발생: $e');
+      return null;
+    }
+  }
+
+
+  void showExitChatDialog(BuildContext context, String chatRoomId) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('채팅방 나가기'),
+          content: Text('정말로 이 채팅방을 나가시겠습니까?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 다이얼로그 닫기
+              },
+              child: Text('취소'),
+            ),
+            TextButton(
+              onPressed: () async {
+                // 채팅방 나가기 함수 호출
+                onExitChatRoom(chatRoomId);
+
+                // 다이얼로그 닫기
+                Navigator.of(context).pop();
+              },
+              child: Text('나가기'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -6,16 +6,16 @@ class ChatScreen extends StatefulWidget {
   final String chatTitle;
   final String chatRoomId;
 
-  ChatScreen({required this.chatTitle, required this.chatRoomId});
+  ChatScreen({required this.chatTitle, required this.chatRoomId, required String uploaderUID, required String winningBidderUID});
 
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  TextEditingController _messageController = TextEditingController(); // 메시지 보내기 함수
+  TextEditingController _messageController = TextEditingController();
   CollectionReference _chatCollection = FirebaseFirestore.instance.collection('chat_messages');
-  ScrollController _scrollController = ScrollController();  // 채팅 메시지가 많아 화면에 다 표시되지 않을 시 스크롤 표시 함수
+  ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +33,13 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
       body: Column(
         children: [
+          Container(
+            color: Colors.lightBlue, // appBar와 _buildMessageInputField 사이의 색을 하늘색으로 설정
+            child: SizedBox(height: 8), // 위젯 간격 조절을 위한 SizedBox
+          ),
           Expanded(
             child: SingleChildScrollView(
-              reverse: true,  // 메시지가 맨 아래에서부터 생성되도록 설정
+              reverse: true,
               controller: _scrollController,
               child: Column(
                 children: [
@@ -78,30 +82,34 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
-  // 각 메시지를 표시하는 위젯
   Widget _buildMessage(String message, String sender) {
-    bool isUser = sender == FirebaseAuth.instance.currentUser?.uid; // 사용자 인증
+    bool isUser = sender == FirebaseAuth.instance.currentUser?.uid;
+
+    Color backgroundColor = isUser ? Colors.yellow : Colors.white;
+    Color textColor = isUser ? Colors.black : Colors.black;
 
     return Align(
-      // 로그인된 사용자가 보낸 메시지는 오른쪽, 상대방이 보낸 메시지는 왼쪽에 뜨게 설정
       alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
           padding: EdgeInsets.all(12),
           decoration: BoxDecoration(
-            color: isUser ? Colors.blue : Colors.green, // 본인 메시지 박스 : 파란색, 상대방 메시지 박스 : 초록색으로 설정
+            color: backgroundColor,
             borderRadius: BorderRadius.circular(8),
-          ), // 채팅방 디자인은 추후 수정 예정
+          ),
           child: Text(
             message,
-            style: TextStyle(color: Colors.white),
+            style: TextStyle(
+              color: textColor,
+              fontSize: 18.0, // 글씨 크기 조절
+            ),
           ),
         ),
       ),
     );
   }
-  // 메시지 입력 필드 위젯
+
   Widget _buildMessageInputField() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -111,7 +119,7 @@ class _ChatScreenState extends State<ChatScreen> {
             child: TextField(
               controller: _messageController,
               decoration: InputDecoration(
-                hintText: '메시지를 입력하세요...',  // 메시지 입력 요청 텍스트
+                hintText: '메시지를 입력하세요...',
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
                 ),
@@ -122,7 +130,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
             icon: Icon(Icons.send),
             onPressed: () {
-              _sendMessage(_messageController.text);  // 메시지를 보낼 시 채팅방에 날리기 설정
+              _sendMessage(_messageController.text);
             },
           ),
         ],
@@ -132,10 +140,10 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void _sendMessage(String message) {
     if (message.isNotEmpty) {
-      User? user = FirebaseAuth.instance.currentUser; // 로그인된 사용자의 정보
+      User? user = FirebaseAuth.instance.currentUser;
 
       if (user != null) {
-        String senderId = user.uid; // UID를 senderId 변수에 할당
+        String senderId = user.uid;
 
         _chatCollection
             .doc(widget.chatTitle)
@@ -146,9 +154,9 @@ class _ChatScreenState extends State<ChatScreen> {
           'timestamp': FieldValue.serverTimestamp(),
         })
             .then((_) {
-          _messageController.clear(); // 메시지 입력 완료 시 메시지 입력 필드 비움
+          _messageController.clear();
           _scrollController.animateTo(
-            0.0,  // 메시지 입력 완료 시 최신 메시지로 스크롤 바 조정
+            0.0,
             curve: Curves.easeOut,
             duration: const Duration(milliseconds: 300),
           );
@@ -162,12 +170,19 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void _updateSharedChatMessagesDocument(
-      String chatRoomId, String message, String senderId) {
-    FirebaseFirestore.instance.collection('chat_messages').doc(chatRoomId).update({
+  void _updateSharedChatMessagesDocument(String chatRoomId, String message, String senderId) {
+    FirebaseFirestore.instance.collection('chat_messages')
+        .doc(chatRoomId)
+        .set({
       'latest_message': message,
       'latest_message_sender': senderId,
       'latest_message_timestamp': FieldValue.serverTimestamp(),
+    }, SetOptions(merge: true))
+        .then((_) {
+      print('Shared chat messages document created/updated successfully.');
+    })
+        .catchError((error) {
+      print('Error creating/updating shared chat messages document: $error');
     });
   }
 }
